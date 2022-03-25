568fb15647433aae65c9980b9298314d799fdaea
Sergey Nezymaev <sergey.nezymaev@pegasus-consultancy.co.uk>
2022-03-25 15:36:47 +0700

    Improve UpdateUserPassword command (#22844)

--- app/bundles/CoreBundle/Command/UpdateAdminPasswordCommand.php
+++ app/bundles/CoreBundle/Command/UpdateAdminPasswordCommand.php
@@ -26,23 +26,35 @@ class UpdateAdminPasswordCommand extends ContainerAwareCommand
     {
 
         $this->setName('mautic:user:change_admin_password')
-            ->setDescription('Change password for admin (first) user')
+            ->setDescription('Change password for user')
             ->setDefinition([
                 new InputOption(
                     'password', null, InputOption::VALUE_OPTIONAL,
-                    'New Admin Password (default is FooBar(9)?', 'FooBar(9)?'
-                )
+                    'New Admin Password', 'FooBar(9)?'
+                ),
+                new InputOption(
+                    'user_id', null, InputOption::VALUE_OPTIONAL,
+                    'ID of user which should be changed', '1'
+                ),
+                new InputOption(
+                    'user_login', null, InputOption::VALUE_OPTIONAL,
+                    'Login of user which should be changed', 'admin'
+                ),
             ])
             ->setHelp(<<<'EOT'
-The <info>%command.name%</info> command change admin password (for user with ID = 1) to new one.
+The <info>%command.name%</info> command change admin password (for user specified user) to new one.
 
 <info>php %command.full_name%</info>
 
-You may specify the ne password via the --password parameter:
+You may specify the new password via the --password parameter:
 
-<info>php %command.full_name% --password=<password>
+<info>php %command.full_name% --password=<password> --user_id=1|--user_login=admin
 
-NOTE: Default Password value is FooBar(9)?
+NOTE: Default Password value is 'FooBar(9)?'
+--user_id - ID of user to change password. Default is 1
+--user_login - Login of user to change password. Default is 'admin'
+
+NOTE: ID parameter will be used first to find user to change password
 </info>
 
 EOT
@@ -53,23 +65,34 @@ EOT
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
         $options       = $input->getOptions();
-        $password     = $options['password'];
+        $password      = $options['password'];
+        $user_id       = $options['user_id'] ?: 1;
+        $user_login    = $options['user_login'] ?: 'admin';
 
-        $output->writeln("\n\n<info>Change password for user with ID = 1</info>");
+        $output->writeln("\n\n<info>Change password for user with ID = {$user_id} and login {$user_login}</info>");
 
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $adminUser = $em->getRepository('MauticUserBundle:User')->find(1);
+        $repo = $em->getRepository('MauticUserBundle:User');
+        $adminUser = $repo->find($user_id);
+
+        if (is_null($adminUser)) {
+            $output->writeln("\n\n<info>Unable to find user by ID. Try to find by login ({$user_login})...</info>");
+            $adminUser =  $repo->findOneBy(array('username' => $user_login));
+            if (is_null($adminUser)) {
+                $output->writeln("\n\n<error>Unable to find user</error>");
+                return false;
+            }
+            $output->writeln("\n\n<info>User founded by login ({$user_login})</info>");
+        }
+
         $encoder = $this->getContainer()->get('security.password_encoder');
         $adminUser->setPassword($encoder->encodePassword($adminUser, $password));
-
-//         $userRepository = $em->getRepository(User::class);
 
         $em->persist($adminUser);
         $em->flush();
 
-        $output->writeln("\n\n<info>User with ID = 1 updated</info>");
-        return 0;
+        $output->writeln("\n\n<info>User with ID = {$adminUser->getId()} and/or login = {$adminUser->getUsername()} updated</info>");
+        return true;
     }
 }
